f02c067a27f51faa32350cdf53f881b195fd8b36
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2008-06-18 17:33:26 +0000

    bin_mul implemented

--- trunk/src/x86/bin_mul.s
+++ trunk/src/x86/bin_mul.s
@@ -18,22 +18,29 @@
 # <http://www.gnu.org/licenses/>.                                       #
 #########################################################################
 
-# void fpfd32_bin_inc(fpfd32_bin_t *dest);
+# void fpfd32_bin_mul(fpfd32_bin_t *dest,
+#                     const fpfd32_bin_t *lhs, const fpfd32_bin_t *rhs);
 #
-# Increment the mantissa, in orter to round. dest must be normalized.
+# Multiply lhs and rhs, and put the result in dest.
 
-.globl fpfd32_bin_inc
-fpfd32_bin_inc:
-        movl 4(%esp), %eax
-        movl (%eax), %edx
-        andl $0xFFFFFF, %edx
-        addl $1,  %edx
-        cmpl $10000000, %edx
-        je Lrollover
-        movl %edx, (%eax)
+.globl fpfd32_bin_mul
+fpfd32_bin_mul:
+        movl 8(%esp), %ebx
+        movl 12(%esp), %ecx
+        movl (%ebx), %eax
+        movl (%ecx), %edx
+        mull %edx               # Multiply the mantissas
+        movl 8(%ebx), %ebx
+        addl 8(%ecx), %ebx      # Add the exponents
+        movl 4(%esp), %ecx
+        movl %eax, (%ecx)
+        movl %edx, 4(%ecx)
+        movl %ebx, 8(%ecx)      # Store the new mantissa and exponents
+        movl 8(%esp), %eax
+        movl 12(%esp), %ebx
+        movl 12(%eax), %edx
+        xorl 12(%ebx), %edx     # XOR the signs: 1 (...0001) xor -1 (...1111)
+                                # gives -2 (...1110), X xor X gives 0
+        addl $1, %edx           # Add one to go from (-2, 0) to (-1, 1)
+        movl %edx, 16(%ecx)     # Store the sign
         ret
-Lrollover:
-        addl $1,  8(%eax)
-        movl $1000000, (%eax)
-        ret
-        
