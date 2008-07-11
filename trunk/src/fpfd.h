2f81eef5bb903e03a4e57f3804fece1a320c3788
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2008-07-11 16:56:02 +0000

    Restructuring dpd/bin

--- trunk/src/fpfd.h
+++ trunk/src/fpfd.h
@@ -25,115 +25,43 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif // __cplusplus
+#endif /* __cplusplus */
 
 typedef enum {
   FPFD_RNDN, FPFD_RNDNA, FPFD_RNDZ, FPFD_RNDU, FPFD_RNDD
 } fpfd_rnd_t;
 
-typedef enum {
-  FPFD_ENCD, FPFD_ENCB
-} fpfd_enc_t;
+typedef uint8_t fpfd32_t[4];
 
-typedef struct {
-  unsigned char data[4];
-  fpfd_enc_t enc;
-} fpfd32_struct_t;
+typedef       uint_8 *fpfd32_ptr;
+typedef const uint_8 *fpfd32_srcptr;
 
-typedef struct {
-  unsigned char data[8];
-  fpfd_enc_t enc;
-} fpfd64_struct_t;
+/* Assignment */
 
-typedef struct {
-  unsigned char data[16];
-  fpfd_enc_t enc;
-} fpfd128_struct_t;
+void fpfd32_set(fpfd32_ptr dest, fpfd32_srcptr src);
 
-typedef fpfd32_struct_t  fpfd32_t[1];
-typedef fpfd64_struct_t  fpfd64_t[1];
-typedef fpfd128_struct_t fpfd128_t[1];
-
-typedef       fpfd32_struct_t  *fpfd32_ptr;
-typedef const fpfd32_struct_t  *fpfd32_srcptr;
-typedef       fpfd64_struct_t  *fpfd64_ptr;
-typedef const fpfd64_struct_t  *fpfd64_srcptr;
-typedef       fpfd128_struct_t *fpfd128_ptr;
-typedef const fpfd128_struct_t *fpfd128_srcptr;
-
-// Assignment
-
-void fpfd32_set32(fpfd32_ptr dest, fpfd32_srcptr src, fpfd_enc_t enc);
-int fpfd32_set64(fpfd32_ptr dest, fpfd64_srcptr src,
-                 fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd32_set128(fpfd32_ptr dest, fpfd128_srcptr src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-
-int fpfd64_set32(fpfd64_ptr dest, fpfd32_srcptr src,
-                 fpfd_rnd_t rnd, fpfd_enc_t enc);
-void fpfd64_set64(fpfd64_ptr dest, fpfd64_srcptr src, fpfd_enc_t enc);
-int fpfd64_set128(fpfd64_ptr dest, fpfd128_srcptr src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-
-int fpfd128_set32(fpfd128_ptr dest, fpfd32_srcptr src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd128_set64(fpfd128_ptr dest, fpfd64_srcptr src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-void fpfd128_set128(fpfd128_ptr dest, fpfd128_srcptr src, fpfd_enc_t enc);
-
-int fpfd32_set_ui(fpfd32_ptr dest, uint32_t src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd32_set_si(fpfd32_ptr dest, int32_t src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-
-int fpfd64_set_ui(fpfd32_ptr dest, uint32_t src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd64_set_si(fpfd32_ptr dest, int32_t src,
-                  fpfd_rnd_t rnd, fpfd_enc_t enc);
-
-int fpfd128_set_ui(fpfd32_ptr dest, uint32_t src,
-                   fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd128_set_si(fpfd32_ptr dest, int32_t src,
-                   fpfd_rnd_t rnd, fpfd_enc_t enc);
-
-// Addition
+/* Addition */
 
 int fpfd32_add(fpfd32_ptr dest, fpfd32_srcptr lhs, fpfd32_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd64_add(fpfd64_ptr dest, fpfd64_srcptr lhs, fpfd64_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd128_add(fpfd128_ptr dest, fpfd128_srcptr lhs, fpfd128_srcptr rhs,
-                fpfd_rnd_t rnd, fpfd_enc_t enc);
+               fpfd_rnd_t rnd);
 
-// Subtraction
+/* Subtraction */
 
 int fpfd32_sub(fpfd32_ptr dest, fpfd32_srcptr lhs, fpfd32_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd64_sub(fpfd64_ptr dest, fpfd64_srcptr lhs, fpfd64_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd128_sub(fpfd128_ptr dest, fpfd128_srcptr lhs, fpfd128_srcptr rhs,
-                fpfd_rnd_t rnd, fpfd_enc_t enc);
+               fpfd_rnd_t rnd);
 
-// Multiplication
+/* Multiplication */
 
 int fpfd32_mul(fpfd32_ptr dest, fpfd32_srcptr lhs, fpfd32_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd64_mul(fpfd64_ptr dest, fpfd64_srcptr lhs, fpfd64_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd128_mul(fpfd128_ptr dest, fpfd128_srcptr lhs, fpfd128_srcptr rhs,
-                fpfd_rnd_t rnd, fpfd_enc_t enc);
+               fpfd_rnd_t rnd);
 
-// Division
+/* Division */
 
 int fpfd32_div(fpfd32_ptr dest, fpfd32_srcptr lhs, fpfd32_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd64_div(fpfd64_ptr dest, fpfd64_srcptr lhs, fpfd64_srcptr rhs,
-               fpfd_rnd_t rnd, fpfd_enc_t enc);
-int fpfd128_div(fpfd128_ptr dest, fpfd128_srcptr lhs, fpfd128_srcptr rhs,
-                fpfd_rnd_t rnd, fpfd_enc_t enc);
+               fpfd_rnd_t rnd);
 
 #ifdef __cplusplus
 }
-#endif // __cplusplus
+#endif /* __cplusplus */
 
-#endif // FPFD_H
+#endif /* FPFD_H */
--- trunk/src/fpfd_impl.h
+++ trunk/src/fpfd_impl.h
@@ -29,14 +29,12 @@ extern "C" {
 #endif // __cplusplus
 
 typedef enum {
-  FPFD_NUMBER, FPFD_SNAN, FPFD_QNAN, FPFD_INF
+  FPFD_ZERO, FPFD_NUMBER, FPFD_SNAN, FPFD_QNAN, FPFD_INF
 } fpfd_special_t;
 
 /* These structs represent expanded versions of the coresponding fpfdX_t.
  * Each actually has room for a mantissa twice the length needed to represent a
- * fpfdX_t, so that multiplication can be done exactly. fpfdX_bcd_t stores a
- * binary-coded-decimal significand, while fpfdX_bin_t stores a binary
- * significand.
+ * fpfdX_t, so that multiplication can be done exactly.
  */
 
 typedef struct {
@@ -44,42 +42,7 @@ typedef struct {
   int32_t exp;
   int32_t sign;
   fpfd_special_t special;
-} fpfd32_bcd_t;
-
-typedef struct {
-  unsigned char mant[8];
-  int32_t exp;
-  int32_t sign;
-  fpfd_special_t special;
-} fpfd32_bin_t;
-
-typedef struct {
-  unsigned char mant[16];
-  int32_t exp;
-  int32_t sign;
-  fpfd_special_t special;
-} fpfd64_bcd_t;
-
-typedef struct {
-  unsigned char mant[16];
-  int32_t exp;
-  int32_t sign;
-  fpfd_special_t special;
-} fpfd64_bin_t;
-
-typedef struct {
-  unsigned char mant[32];
-  int32_t exp;
-  int32_t sign;
-  fpfd_special_t special;
-} fpfd128_bcd_t;
-
-typedef struct {
-  unsigned char mant[32];
-  int32_t exp;
-  int32_t sign;
-  fpfd_special_t special;
-} fpfd128_bin_t;
+} fpfd32_expanded_t;
 
 /* These routines work on expanded fpfdX_t's. A void return type signifies that
  * the result is always exact. Otherwise, the functions return a remainder equal
@@ -90,63 +53,30 @@ typedef struct {
  * correct ternary values, and round the result correctly.
  */
 
-/* Simple routines */
+void fpfd32_expand(fpfd32_expanded_t *dest, fpfd32_srcptr src);
+void fpfd32_contract(fpfd32_ptr dest, const fpfd32_expanded_t *src);
 
-void fpfd32_to_bcd(fpfd32_bcd_t *dest, fpfd32_srcptr src);
-void fpfd32_to_bin(fpfd32_bin_t *dest, fpfd32_srcptr src);
+void fpfd32_impl_inc(fpfd32_expanded_t *dest);
 
-void fpfd32_from_bcd(fpfd32_ptr dest, const fpfd32_bcd_t *src);
-void fpfd32_from_bin(fpfd32_ptr dest, const fpfd32_bin_t *src);
+uint32_t fpfd32_impl_addsub(fpfd32_expanded_t *dest, int32_t sign,
+                            const fpfd32_expanded_t *lhs,
+                            const fpfd32_expanded_t *rhs);
 
-void fpfd32_bcd_inc(fpfd32_bcd_t *dest);
-void fpfd32_bin_inc(fpfd32_bin_t *dest);
+void fpfd32_impl_mul(fpfd32_expanded_t *dest,
+                     const fpfd32_expanded_t *lhs,
+                     const fpfd32_expanded_t *rhs);
 
-uint32_t fpfd32_bcd_addsub(fpfd32_bcd_t *dest, int32_t sign,
-                           const fpfd32_bcd_t *lhs, const fpfd32_bcd_t *rhs);
+uint32_t fpfd32_impl_div(fpfd32_expanded_t *dest,
+                         const fpfd32_expanded_t *lhs,
+                         const fpfd32_expanded_t *rhs);
 
-void fpfd32_bin_mul(fpfd32_bin_t *dest,
-                    const fpfd32_bin_t *lhs, const fpfd32_bin_t *rhs);
-uint32_t fpfd32_bin_div(fpfd32_bin_t *dest,
-                        const fpfd32_bin_t *lhs, const fpfd32_bin_t *rhs);
-
-uint32_t fpfd32_bcd_normalize(fpfd32_bcd_t *dest);
-
-/* Expensive routines */
-
-void fpfd32_bcd_to_bin(fpfd32_bin_t *dest, const fpfd32_bcd_t *src);
-void fpfd32_bin_to_bcd(fpfd32_bcd_t *dest, const fpfd32_bin_t *src);
-
-uint32_t fpfd32_bin_addsub(fpfd32_bin_t *dest, int32_t sign,
-                           const fpfd32_bin_t *lhs, const fpfd32_bin_t *rhs);
-
-void fpfd32_bcd_mul(fpfd32_bcd_t *dest,
-                    const fpfd32_bcd_t *lhs, const fpfd32_bcd_t *rhs);
-uint32_t fpfd32_bcd_div(fpfd32_bcd_t *dest,
-                        const fpfd32_bcd_t *lhs, const fpfd32_bcd_t *rhs);
-
-uint32_t fpfd32_bin_normalize(fpfd32_bin_t *dest);
+uint32_t fpfd32_impl_normalize(fpfd32_expanded_t *dest);
 
 /* Help with correct rounding */
 
-int fpfd32_bcd_tern(fpfd32_bcd_t *dest, uint32_t rem, fpfd_rnd_t rnd);
-int fpfd32_bcd_tern2(fpfd32_bcd_t *dest, uint32_t rem1, uint32_t rem2,
-                     fpfd_rnd_t rnd);
-
-int fpfd32_bin_tern(fpfd32_bin_t *dest, uint32_t rem, fpfd_rnd_t rnd);
-int fpfd32_bin_tern2(fpfd32_bin_t *dest, uint32_t rem1, uint32_t rem2,
-                     fpfd_rnd_t rnd);
-
-/* Expand more than one input into the same format */
-void fpfd32_expand2(fpfd32_srcptr fp1, fpfd32_srcptr fp2,
-                    fpfd32_bcd_t *bcd1, fpfd32_bcd_t *bcd2,
-                    fpfd32_bin_t *bin1, fpfd32_bin_t *bin2,
-                    fpfd_enc_t enc);
-
-/* Propagate NANs if one or more operands is NAN */
-int fpfd32_bcd_nanprop(fpfd32_bcd_t *bcd,
-                       fpfd32_bcd_t *bcd1, fpfd32_bcd_t *bcd2);
-int fpfd32_bin_nanprop(fpfd32_bin_t *bin,
-                       fpfd32_bin_t *bin1, fpfd32_bin_t *bin2);
+int fpfd32_impl_tern(fpfd32_expanded_t *dest, uint32_t rem, fpfd_rnd_t rnd);
+int fpfd32_impl_tern2(fpfd32_expanded_t *dest, uint32_t rem1, uint32_t rem2,
+                      fpfd_rnd_t rnd);
 
 /* Bail out of bad situations. This function doesn't return. */
 void fpfd_panic(const char *error);
