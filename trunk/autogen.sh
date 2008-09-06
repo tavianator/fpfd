09358adb18ce5fcd00856667772ea641a6b4ed2b
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2008-09-06 23:42:52 +0000

    Make x86 not conflict with x86_64

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -4,5 +4,5 @@ aclocal
 autoconf
 libtoolize --force --copy
 automake --add-missing --copy
-libfpfd-dpd/x86/dpdtable.sh > libfpfd-dpd/x86/dpdtable.s
+libfpfd-dpd/x86/dpdtable-x86.sh > libfpfd-dpd/x86/dpdtable-x86.s
 echo You may now run ./configure
