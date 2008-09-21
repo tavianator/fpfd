95d79a6fba10e77d0ca726a8fcaf220fef4cac96
Tavian Barnes <tavianator@tavianator.com>
2008-09-21 00:10:39 +0000

    impl_scale test improvements

--- check/scale-dpd.c
+++ check/scale-dpd.c
@@ -43,55 +43,55 @@ main()
    * to be passed to fpfd*_impl_scale during normal operation.
    */
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(1));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x1));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, -6, 0);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(1000000));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x1000000));
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(999999));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x999999));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, -1, 0);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(9999990));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x9999990));
 
   /*
    * These values are within the scaled coefficient range, and thus should not
    * be altered by a call to fpfd*_impl_scale.
    */
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(1000000));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x1000000));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, 0, 0);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(1000000));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x1000000));
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(9999999));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x9999999));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, 0, 0);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(9999999));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x9999999));
 
   /*
    * These values require scaling down, but have the high 32 bits of their
    * coefficient set to zero. They are treated specially on 32-bit hardware.
    */
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(10000000));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x10000000));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, 1, 0);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(1000000));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x1000000));
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(10000001));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x10000001));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, 1, 1);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(1000000));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x1000000));
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(99999995));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x99999995));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, 1, 5);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(9999999));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x9999999));
 
-  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(99999999));
+  fpfd32_impl_set_manually(&impl32, UINT32_C(0), UINT32_C(0x99999999));
   impl32.fields.exp = 0;
   fpfd_impl_assert_orev(impl_scale, &impl, 1, 9);
-  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(9999999));
+  fpfd32_impl_assert_mant(&impl32, UINT32_C(0), UINT32_C(0x9999999));
 
   return exitstatus;
 }
