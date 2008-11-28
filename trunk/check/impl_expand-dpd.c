95ae70ff2d765091fa2ede85eeb05f47f75e861e
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2008-11-28 22:28:03 +0000

    Wrote beginning of DPD impl_expand test.

--- trunk/check/impl_expand-dpd.c
+++ trunk/check/impl_expand-dpd.c
@@ -25,5 +25,53 @@
 int
 main()
 {
+  /* Canonical inputs */
+
+  fpfd_declare(zero);
+  fpfd_declare(one);
+  fpfd_declare(nines);
+  fpfd_declare(inf);
+  fpfd_declare(sNaN);
+  fpfd_declare(qNaN);
+
+  fpfd_impl_declare(zero_impl);
+  fpfd_impl_declare(one_impl);
+  fpfd_impl_declare(nines_impl);
+  fpfd_impl_declare(inf_impl);
+  fpfd_impl_declare(sNaN_impl);
+  fpfd_impl_declare(qNaN_impl);
+
+  /*
+   * Test that fpfd*_impl_expand works for 0, 1, 9999999, +inf, sNan, and qNaN.
+   */
+
+  fpfd32_set_manually(zero32, UINT32_C(0x21400000));
+  fpfd_impl_assert_ora1sf(impl_expand, &zero_impl, zero,
+                          1, FPFD_ZERO);
+
+  fpfd32_set_manually(one32, UINT32_C(0x26500001));
+  fpfd_impl_assert_ora1esf(impl_expand, &one_impl, one,
+                           0, 1, FPFD_NUMBER);
+  fpfd32_impl_assert_mant(&one_impl32, UINT32_C(0), UINT32_C(0x1));
+
+  fpfd32_set_manually(nines32, UINT32_C(0x6853FCFF));
+  fpfd_impl_assert_ora1esf(impl_expand, &nines_impl, nines,
+                           0, 1, FPFD_NUMBER);
+  fpfd32_impl_assert_mant(&nines_impl32, UINT32_C(0), UINT32_C(0x9999999));
+
+  fpfd32_set_manually(inf32, UINT32_C(0x78000000));
+  fpfd_impl_assert_ora1sf(impl_expand, &inf_impl, inf,
+                          1, FPFD_INF);
+
+  fpfd32_set_manually(sNaN32, UINT32_C(0x7E028E56));
+  fpfd_impl_assert_ora1sf(impl_expand, &sNaN_impl, sNaN,
+                          1, FPFD_SNAN);
+  fpfd32_impl_assert_mant(&sNaN_impl32, UINT32_C(0), UINT32_C(0x123456));
+
+  fpfd32_set_manually(qNaN32, UINT32_C(0x7C028E56));
+  fpfd_impl_assert_ora1sf(impl_expand, &qNaN_impl, qNaN,
+                          1, FPFD_QNAN);
+  fpfd32_impl_assert_mant(&qNaN_impl32, UINT32_C(0), UINT32_C(0x123456));
+  
   return exitstatus;
 }
