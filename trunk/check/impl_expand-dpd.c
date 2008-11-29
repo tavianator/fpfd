d5e295a9c79a42fa0f93693eea9156293a72e175
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2008-11-29 23:49:04 +0000

    Finish DPD impl_expand test.

--- trunk/check/impl_expand-dpd.c
+++ trunk/check/impl_expand-dpd.c
@@ -43,6 +43,16 @@ main()
   fpfd_impl_declare(sNaN_impl);
   fpfd_impl_declare(qNaN_impl);
 
+  /* Non-canonical inputs */
+
+  fpfd_declare(noncanon);
+  fpfd_declare(noncanon_sNaN);
+  fpfd_declare(noncanon_qNaN);
+
+  fpfd_impl_declare(noncanon_impl);
+  fpfd_impl_declare(noncanon_sNaN_impl);
+  fpfd_impl_declare(noncanon_qNaN_impl);
+
   /*
    * Test that fpfd*_impl_expand works for 0, 1, 1111111, 9999999, +inf, sNan,
    * and qNaN.
@@ -80,6 +90,28 @@ main()
   fpfd_impl_assert_ora1sf(impl_expand, &qNaN_impl, qNaN,
                           1, FPFD_QNAN);
   fpfd32_impl_assert_mant(&qNaN_impl32, UINT32_C(0), UINT32_C(0x123456));
-  
+
+  /*
+   * Non-canonical declets should be handled according to Table 3.3, IEEE-2008
+   * S3.5.2, p12.
+   */
+
+  fpfd32_set_manually(noncanon32, UINT32_C(0x2655BAFF));
+  fpfd_impl_assert_ora1esf(impl_expand, &noncanon_impl, noncanon,
+                           0, 1, FPFD_NUMBER);
+  fpfd32_impl_assert_mant(&noncanon_impl32, UINT32_C(0), UINT32_C(0x1888999));
+
+  fpfd32_set_manually(noncanon_sNaN32, UINT32_C(0x7E0DB9FF));
+  fpfd_impl_assert_ora1sf(impl_expand, &noncanon_sNaN_impl, noncanon_sNaN,
+                          1, FPFD_SNAN);
+  fpfd32_impl_assert_mant(&noncanon_sNaN_impl32,
+                          UINT32_C(0), UINT32_C(0x888999));
+
+  fpfd32_set_manually(noncanon_qNaN32, UINT32_C(0x7C09BBFF));
+  fpfd_impl_assert_ora1sf(impl_expand, &noncanon_qNaN_impl, noncanon_qNaN,
+                          1, FPFD_QNAN);
+  fpfd32_impl_assert_mant(&noncanon_qNaN_impl32,
+                          UINT32_C(0), UINT32_C(0x888999));
+
   return exitstatus;
 }
