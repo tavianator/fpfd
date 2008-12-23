432dc23422dfe1f7aee56ea3fa789de498e6a8fd
Tavian Barnes <tavianator@tavianator.com>
2008-12-23 22:30:12 +0000

    Yet more work on DPD impl_addsub.

--- libfpfd-dpd/x86_64/addsub-x86_64.s
+++ libfpfd-dpd/x86_64/addsub-x86_64.s
@@ -40,31 +40,40 @@ fpfd32_impl_addsub:
         bsrq 16(%rsp), %r9
         subl $60, %r8d
         subl $60, %r9d
-        shrl $2, %r8d
-        shrl $2, %r9d
         negl %r8d
         negl %r9d
-        movl %r9d, %ecx
-        subl %r8d, %ecx
-        addl 8(%rsp), %ecx
-        subl 24(%rsp), %ecx
+        shrl $2, %r8d
+        shrl $2, %r9d
+        movl %r9d, %edx
+        subl %r8d, %edx
+        addl 8(%rsp), %edx
+        subl 24(%rsp), %edx
         jns .Laddnoswitch
         movq 16(%rsp), %rax
         xchgq (%rsp), %rax
         movq %rax, 16(%rsp)
-        movq 24(%rsp), %rax
-        xchgq 8(%rsp), %rax
-        movq %rax, 24(%rsp)
+        movl 24(%rsp), %eax
+        xchgl 8(%rsp), %eax
+        movl %eax, 24(%rsp)
         xchgl %r8d, %r9d
-        negl %ecx
+        negl %edx
 .Laddnoswitch:
         movq (%rsp), %rax
         leal (,%r8d,4), %ecx
         shlq %cl, %rax
-        movq %rax, (%rdi)
-        movq 8(%rsp), %rax
+        subl %edx, %r9d
+        js .Laddnoadd
+        leal (,%r9d,4), %ecx
+        movq 16(%rsp), %rdx
+        shlq %cl, %rdx
+        addq %rdx, %rax
+.Laddnoadd:
+        movq %rax, (%rdi)       # Save the mantissa
+        movl 8(%rsp), %eax
         addl %r8d, %eax
-        movq %rax, 8(%rdi)
+        movl %eax, 8(%rdi)      # Adjust and save the exponent
+        movl 12(%rsp), %eax
+        movl %eax, 12(%rdi)     # Save the sign
         movl $1, 16(%rdi)       # Set the special flag to FPFD_NUMBER
         addq $32, %rsp
         ret
