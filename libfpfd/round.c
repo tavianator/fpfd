239e851c021f839ffde0d261e214c510e05d8013
Tavian Barnes <tavianator@tavianator.com>
2009-01-31 05:56:38 +0000

    Fix rounding of operations with a zero result.

--- libfpfd/round.c
+++ libfpfd/round.c
@@ -33,7 +33,7 @@ typedef enum {
   FPFD_RZERO, FPFD_RSIGN, FPFD_RMSIGN, FPFD_RMSIGNO
 } fpfd_round_action_t;
 
-static fpfd_round_action_t fpfd_round(int sign, unsigned int rem,
+static fpfd_round_action_t fpfd_round(fpfd_impl_t *fields, unsigned int rem,
                                       fpfd_rnd_t rnd, fpfd_flags_t *flags);
 
 int
@@ -43,7 +43,7 @@ fpfd32_impl_round(fpfd32_impl_t *dest, unsigned int rem, fpfd_rnd_t rnd,
   int tern;
   fpfd_round_action_t action;
 
-  action = fpfd_round(dest->fields.sign, rem, rnd, flags);
+  action = fpfd_round(&dest->fields, rem, rnd, flags);
 
   switch (action) {
   case FPFD_RZERO:
@@ -82,87 +82,93 @@ fpfd32_impl_round2(fpfd32_impl_t *dest,
 }
 
 static fpfd_round_action_t
-fpfd_round(int sign, unsigned int rem, fpfd_rnd_t rnd, fpfd_flags_t *flags)
+fpfd_round(fpfd_impl_t *fields, unsigned int rem, fpfd_rnd_t rnd,
+           fpfd_flags_t *flags)
 {
   fpfd_round_action_t action = FPFD_RZERO;
 
-  if (flags) {
-    if ((rem & 0xF) != 0 && (rem & 0xF) != 10) *flags |= FPFD_INEXACT;
-    if ((rem | 0x10) && rem != (10 | 0x10)) *flags |= FPFD_UNDERFLOW;
-    if (rem == 10) *flags |= FPFD_OVERFLOW;
-  }
-
-  if ((rem & 0xF) == 0) {
-    action = FPFD_RZERO;
-  } else if (rem == 10) {
-    switch (rnd) {
-    case FPFD_RNDN:
-    case FPFD_RNDNA:
-      action = FPFD_RMSIGN;
-      break;
-
-    case FPFD_RNDZ:
-      action = FPFD_RSIGN;
-      break;
-
-    case FPFD_RNDU:
-      if (sign > 0) {
-        action = FPFD_RMSIGN;
-      } else {
-        action = FPFD_RSIGN;
-      }
-      break;
-
-    case FPFD_RNDD:
-      if (sign < 0) {
-        action = FPFD_RMSIGN;
-      } else {
-        action = FPFD_RSIGN;
-      }
-      break;
+  if (fields->special == FPFD_ZERO) {
+    if (rnd == FPFD_RNDD) fields->sign = -1;
+    else fields->sign = 1;
+  } else if (fields->special == FPFD_NUMBER) {
+    if (flags) {
+      if ((rem & 0xF) != 0 && (rem & 0xF) != 10) *flags |= FPFD_INEXACT;
+      if ((rem | 0x10) && rem != (10 | 0x10)) *flags |= FPFD_UNDERFLOW;
+      if (rem == 10) *flags |= FPFD_OVERFLOW;
     }
-  } else if (rem == (10 | 0x10)) {
-    action = FPFD_RSIGN;
-  } else {
-    rem &= 0xF;
 
-    switch (rnd) {
-    case FPFD_RNDN:
-      if (rem < 5) {
-        action = FPFD_RSIGN;
-      } else if (rem == 5) {
-        action = FPFD_RMSIGNO;
-      } else {
+    if ((rem & 0xF) == 0) {
+      action = FPFD_RZERO;
+    } else if (rem == 10) {
+      switch (rnd) {
+      case FPFD_RNDN:
+      case FPFD_RNDNA:
         action = FPFD_RMSIGN;
-      }
-      break;
+        break;
 
-    case FPFD_RNDNA:
-      if (rem < 5) {
+      case FPFD_RNDZ:
         action = FPFD_RSIGN;
-      } else {
-        action = FPFD_RMSIGN;
-      }
+        break;
 
-    case FPFD_RNDZ:
+      case FPFD_RNDU:
+        if (fields->sign > 0) {
+          action = FPFD_RMSIGN;
+        } else {
+          action = FPFD_RSIGN;
+        }
+        break;
+
+      case FPFD_RNDD:
+        if (fields->sign < 0) {
+          action = FPFD_RMSIGN;
+        } else {
+          action = FPFD_RSIGN;
+        }
+        break;
+      }
+    } else if (rem == (10 | 0x10)) {
       action = FPFD_RSIGN;
-      break;
+    } else {
+      rem &= 0xF;
 
-    case FPFD_RNDU:
-      if (sign > 0) {
-        action = FPFD_RMSIGN;
-      } else {
-        action = FPFD_RSIGN;
-      }
-      break;
+      switch (rnd) {
+      case FPFD_RNDN:
+        if (rem < 5) {
+          action = FPFD_RSIGN;
+        } else if (rem == 5) {
+          action = FPFD_RMSIGNO;
+        } else {
+          action = FPFD_RMSIGN;
+        }
+        break;
 
-    case FPFD_RNDD:
-      if (sign < 0) {
-        action = FPFD_RMSIGN;
-      } else {
+      case FPFD_RNDNA:
+        if (rem < 5) {
+          action = FPFD_RSIGN;
+        } else {
+          action = FPFD_RMSIGN;
+        }
+
+      case FPFD_RNDZ:
         action = FPFD_RSIGN;
+        break;
+
+      case FPFD_RNDU:
+        if (fields->sign > 0) {
+          action = FPFD_RMSIGN;
+        } else {
+          action = FPFD_RSIGN;
+        }
+        break;
+
+      case FPFD_RNDD:
+        if (fields->sign < 0) {
+          action = FPFD_RMSIGN;
+        } else {
+          action = FPFD_RSIGN;
+        }
+        break;
       }
-      break;
     }
   }
 
