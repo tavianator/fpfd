597c51d99e7227dd5c50f707128491deb40f6eef
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2009-03-01 17:27:51 +0000

    Provide a way to determine the current encoding: fpfd_encoding().

---
+++ trunk/libfpfd-bid/x86_64/encoding-x86_64.s
@@ -0,0 +1,33 @@
+/*************************************************************************
+ * Copyright (C) 2008 Tavian Barnes <tavianator@gmail.com>               *
+ *                                                                       *
+ * This file is part of The FPFD Library.                                *
+ *                                                                       *
+ * The FPFD Library is free software; you can redistribute it and/or     *
+ * modify it under the terms of the GNU Lesser General Public License as *
+ * published by the Free Software Foundation; either version 3 of the    *
+ * License, or (at your option) any later version.                       *
+ *                                                                       *
+ * The FPFD Library is distributed in the hope that it will be useful,   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ * Lesser General Public License for more details.                       *
+ *                                                                       *
+ * You should have received a copy of the GNU Lesser General Public      *
+ * License along with this program.  If not, see                         *
+ * <http://www.gnu.org/licenses/>.                                       *
+ *************************************************************************/
+
+/* fpfd_enc_t fpfd_encoding(); */
+
+/*
+ * Return FPFD_ENCBID.
+ */
+
+        .text
+.globl fpfd_encoding
+        .type fpfd_encoding, @function
+fpfd_encoding:
+        movl $1, %eax
+        ret
+        .size fpfd_encoding, .-fpfd_encoding
