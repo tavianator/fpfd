550e6a15a36c487d837c7f131fa432ff91bab5e0
tavianator <tavianator@cad28434-d94a-0410-8e88-ed5ddfa0639c>
2009-03-21 06:02:46 +0000

    Improve plot.sh.

--- trunk/bench/plot.sh
+++ trunk/bench/plot.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-gnuplot -persist <<EOF
-plot '$1' index 0 title '$(basename $1)' with points,         \
+prefix=$(dirname $1 | sed 's|.*/\(.*\)|\1|g' | tr '[:lower:]' '[:upper:]');
+suffix=$(basename $1 .dat)
+
+if [ "$2" = "png" ]; then
+  gnuplot <<EOF
+set lmargin 3.25
+set rmargin 2
+set term png size 665,480 enhanced
+set output '${prefix}_$suffix.png'
+plot '$1' index 0 title '$prefix $suffix' with points,        \
      '$1' index 1 title 'Mean' with lines,                    \
      '$1' index 2:3 title 'One standard deviation' with lines
 EOF
+else
+  gnuplot -persist <<EOF
+set lmargin 3.25
+set rmargin 2
+plot '$1' index 0 title '$prefix $suffix' with points,        \
+     '$1' index 1 title 'Mean' with lines,                    \
+     '$1' index 2:3 title 'One standard deviation' with lines
+EOF
+fi
