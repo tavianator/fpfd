5a8c1965ca627d1388aedf9dc193034f3217f95a
Tavian Barnes <tavianator@tavianator.com>
2009-03-21 23:56:03 +0000

    Change ticks*.s in benchmark suite to use /* comments.

--- bench/x86_64/ticks-x86_64.s
+++ bench/x86_64/ticks-x86_64.s
@@ -1,37 +1,38 @@
-#########################################################################
-# Copyright (C) 2008 Tavian Barnes <tavianator@gmail.com>               #
-#                                                                       #
-# This file is part of The FPFD Library Benchmark Suite.                #
-#                                                                       #
-# The FPFD Library Benchmark Suite is free software; you can            #
-# redistribute it and/or modify it under the terms of the GNU General   #
-# Public License as published by the Free Software Foundation; either   #
-# version 3 of the License, or (at your option) any later version.      #
-#                                                                       #
-# The FPFD Library Benchmark Suite is distributed in the hope that it   #
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied    #
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See #
-# the GNU General Public License for more details.                      #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#########################################################################
+/*************************************************************************
+ * Copyright (C) 2008 Tavian Barnes <tavianator@gmail.com>               *
+ *                                                                       *
+ * This file is part of The FPFD Library Benchmark Suite.                *
+ *                                                                       *
+ * The FPFD Library Benchmark Suite is free software; you can            *
+ * redistribute it and/or modify it under the terms of the GNU General   *
+ * Public License as published by the Free Software Foundation; either   *
+ * version 3 of the License, or (at your option) any later version.      *
+ *                                                                       *
+ * The FPFD Library Benchmark Suite is distributed in the hope that it   *
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied    *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See *
+ * the GNU General Public License for more details.                      *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
 
-# long ticks();
-#
-# Return the time stamp counter, and serialize the instruction
+/*
+ * Return the time stamp counter, and serialize the instruction
+ */
 
         .text
+/* long ticks(); */
 .globl ticks
         .type ticks, @function
 ticks:
-        movq %rbx, %rdi         # Callee-save register, clobbered by cpuid
-        cpuid                   # Serialize
-        rdtsc                   # Read time stamp counter
+        movq %rbx, %rdi         /* Callee-save register, clobbered by cpuid */
+        cpuid                   /* Serialize */
+        rdtsc                   /* Read time stamp counter */
         shlq $32, %rdx
         orq %rdx, %rax
         movq %rax, %rsi
-        cpuid
+        cpuid                   /* Serialize again */
         movq %rsi, %rax
         movq %rdi, %rbx
         ret
