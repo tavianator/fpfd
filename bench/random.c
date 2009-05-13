42af3db7be1f0c294c800290bd96b72cf0c7b6dd
Tavian Barnes <tavianator@tavianator.com>
2009-05-13 22:57:43 +0000

    Format bench/random.c.

--- bench/random.c
+++ bench/random.c
@@ -122,11 +122,15 @@ fpfd128_random(fpfd128_ptr dest)
 static unsigned long next = 1;
 
 /* FPFD_RAND_MAX assumed to be 32767 */
-static unsigned int fpfd_rand() {
+static unsigned int
+fpfd_rand()
+{
   next = next*1103515245 + 12345;
   return (unsigned int)(next/65536) % 32768;
 }
 
-static void fpfd_srand(unsigned int seed) {
+static void
+fpfd_srand(unsigned int seed)
+{
   next = seed;
 }
